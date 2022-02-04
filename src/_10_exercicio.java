52c598bee555b474c0582e3d73562582f7ad412d
João Vitor de Abreu <jvabreusousa14@gmail.com>
2022-02-04 10:51:09 -0300

    scripts da aula do dia 03/02/22

--- src/_10_exercicio.java
+++ src/_10_exercicio.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
 public class _10_exercicio {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.print("Qual o seu salário?: ");
+        double salario = leia.nextDouble();
+
+        if(salario < 1000){
+            System.out.println("Seu salário é: R$" + (salario + (salario * 0.25)));
+        } else{
+            if(salario >= 1000 && salario < 2000){
+                System.out.println("Seu salário é: R$" + (salario + (salario * 0.15)));
+            } else {
+                if (salario >= 2000){
+                    System.out.println("Seu salário é: R$" + (salario + (salario * 0.10)));
+                }
+            }
+        }
+    }
 }
